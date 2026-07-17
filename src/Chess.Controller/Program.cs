54e85c3c75e81be3a71c894ecd1b831bdacb71fe
Zaido219 <jpmalbas4@gmail.com>
2026-07-17 19:34:25 +0800

    feat:unicode mapping for the pieces

--- src/Chess.Controller/Program.cs
+++ src/Chess.Controller/Program.cs
@@ -1,1 +1,3 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// console doesnt support  unicode out of the box
+System.Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Hello, World!");
--- src/Chess.UI/BoardRenderer.cs
+++ src/Chess.UI/BoardRenderer.cs
@@ -8,4 +8,28 @@ public class BoardRenderer
     {
         
     }
+    private string mapUnicode(int piece)
+    {
+        return piece switch
+        {
+            // White Pieces
+        (Piece.White | Piece.King)   => "\u2654",
+        (Piece.White | Piece.Queen)  => "\u2655",
+        (Piece.White | Piece.Rook)   => "\u2656",
+        (Piece.White | Piece.Bishop) => "\u2657",
+        (Piece.White | Piece.Knight) => "\u2658",
+        (Piece.White | Piece.Pawn)   => "\u2659",
+
+        // Black Pieces
+        (Piece.Black | Piece.King)   => "\u265A",
+        (Piece.Black | Piece.Queen)  => "\u265B",
+        (Piece.Black | Piece.Rook)   => "\u265C",
+        (Piece.Black | Piece.Bishop) => "\u265D",
+        (Piece.Black | Piece.Knight) => "\u265E",
+        (Piece.Black | Piece.Pawn)   => "\u265F",
+
+        // Empty square or fallback
+        _ => "."
+        };
+    }
 }
