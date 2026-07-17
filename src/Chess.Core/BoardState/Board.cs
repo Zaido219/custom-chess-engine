721cb1c2f34a6d438418992e2e0c01c6965c920b
Zaido219 <jpmalbas4@gmail.com>
2026-07-17 19:12:24 +0800

    feat:bit masking and color identity for the pieces

--- src/Chess.Core/BoardState/Board.cs
+++ src/Chess.Core/BoardState/Board.cs
@@ -3,11 +3,19 @@ public class Board
 {
     //represents board as a array of 64 square
     // will change this to bitboards later on
-    private readonly int[] _squares = new int[64];
+    private readonly int[] _squares;
     // method that will allow chess.ui to query whats piece is on this index
     // without knowing how the board is internally represented
     public int this[int squareIndex]
     {
         get => _squares[squareIndex];
+    }
+
+    public Board()
+    {
+        _squares = new int[64];
+
+        _squares[0] = Piece.White | Piece.Bishop;
+        _squares[63] = Piece.Black | Piece.Queen;
     }
 }
--- src/Chess.Core/BoardState/Pieces.cs
+++ src/Chess.Core/BoardState/Pieces.cs
@@ -12,4 +12,18 @@ public static class Piece
     //colors
     public const int White = 8;
     public const int Black = 16;
+    //bitmask
+    private const int typeMask = 0b00111; // 7 in decimal (covers bits 1, 2, and 4)
+    private const int colorMask = 0b11000; // 24 in decimal (covers bits 8 and 16)
+    // extract the color value 8 for white, 16 for black, 0 for none
+    public static int color(int piece) => piece & colorMask;
+    // extract the type value 1-6
+    public static int pieceType(int piece) => piece & typeMask;
+    // helper to check if piece is of specific color
+    public static bool isColor(int piece, int color) => (piece & colorMask) == color;
 }
+
+// How this works in practice:If we pass our White Bishop (12 or 01100) into these helpers:
+// PieceType(12): Performs 01100 & 00111 $\rightarrow$ results in 00100 ($4$, which is Bishop).
+// Color(12): Performs 01100 & 11000 $\rightarrow$ results in 01000 ($8$, which is White).IsColor(12, 
+// Piece.White): Checks if Color(12) == 8 $\rightarrow$ returns true.
