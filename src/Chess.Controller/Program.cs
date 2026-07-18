b1cfac0b45a0d33246a3327695f73a082e576745
Zaido219 <jpmalbas4@gmail.com>
2026-07-18 15:51:52 +0800

    feat:loading from fen string verified

--- src/Chess.Controller/Program.cs
+++ src/Chess.Controller/Program.cs
@@ -1,10 +1,13 @@
 ﻿using Chess.UI;
 using Chess.Core.BoardState;
-using System.Diagnostics.CodeAnalysis;
 // console doesnt support  unicode out of the box
 System.Console.OutputEncoding = System.Text.Encoding.UTF8;
-
 Board board = new Board();
+FenUtility fenUtility = new FenUtility();
+fenUtility.loadFen("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2", board);
+
 BoardRenderer.Render(board);
+
+// test only
 
 Console.Write("This is a stupid way to remind but, you have turned off smart-control, turn it back-on before ending coding session");
--- src/Chess.Core/BoardState/FenUtility.cs
+++ src/Chess.Core/BoardState/FenUtility.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Microsoft.VisualBasic;
 
 namespace Chess.Core.BoardState;
-
+// this class must change the state of the current board
 public class FenUtility
 {
     // fen map
@@ -24,6 +24,42 @@ public class FenUtility
         ['b'] = Piece.Black | Piece.Bishop,
         ['r'] = Piece.Black | Piece.Rook,
         ['q'] = Piece.Black | Piece.Queen
-
     };
+
+    // parse fen string
+    public void loadFen(string fen, Board board)
+    {
+        // this class needs to return a piece
+        //isolate the piece layouts from active color, castling rights, etc.
+        string fenBoard = fen.Split(' ')[0];
+        Console.WriteLine(fenBoard);
+        // loop through the fen board
+        int file = 0;
+        int rank = 7;
+        foreach (char symbol in fenBoard)
+        {
+            if (symbol == '/')
+            {
+                file = 0;
+                rank--;
+            }
+            else if (char.IsDigit(symbol)) // check if its a number cause its a square count
+            {
+                // move file pointer by that number
+                file += int.Parse(symbol.ToString());
+            }
+            else // current symbol is a letter
+            {
+                // calculate the 1d index
+                int index = (rank * 8) + file;
+                //write the fetch piece to the board
+                board[index] = PieceMap[symbol];
+                // increment file
+                file++;
+            }
+        }
+    }
 }
+
+// Now you can read and write to squares directly using array syntax on the board object:
+//board[0] = PieceMap['R']; // This works because of your internal setter!
