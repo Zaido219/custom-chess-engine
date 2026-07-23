4c6cadd94375a7af4bd0a0baafaed4b2c2333590
Zaido219 <jpmalbas4@gmail.com>
2026-07-23 09:25:09 +0800

    feat:Interface for the drag and drop behaviour implementin classes

--- src/Chess.UI/DragAndDropHandler.cs
+++ src/Chess.UI/DragAndDropHandler.cs
@@ -8,19 +8,19 @@ public class DragAndDropHandler : IDragAndDropHandler
     public bool isDragging { get; }
     public Vector2 currentDragPosition { get; set; }
 
-    void BeginDrawing(int squareIndex, Vector2 mousePosition, Vector2 tileOrigin)
+    public void BeginDrag(int squareIndex, Vector2 mousePosition, Vector2 tileOrigin)
     {
 
     }
-    void UpdateDrag(Vector2 mousePosition)
+    public void UpdateDrag(Vector2 mousePosition)
     {
 
     }
-    void EndDrag(out int squareIndex)
+    public void EndDrag(out int squareIndex)
     {
 
     }
-    void CancelDrag()
+    public void CancelDrag()
     {
 
     }
