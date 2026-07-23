6fabebe08200710ced6e1ba8ba4e5d91cd5d93ca
Zaido219 <jpmalbas4@gmail.com>
2026-07-23 09:40:43 +0800

    feat:WIP Implementing drag and drop behaviour

--- src/Chess.UI/DragAndDropHandler.cs
+++ src/Chess.UI/DragAndDropHandler.cs
@@ -4,17 +4,22 @@ namespace Chess.UI;
 
 public class DragAndDropHandler : IDragAndDropHandler
 {
-    public int selectedSquareIndex { get; }
-    public bool isDragging { get; }
+    public int selectedSquareIndex { get; private set; }
+    public bool isDragging { get; private set; } // public getter; private setter restricts mutation to this class
     public Vector2 currentDragPosition { get; set; }
+    private Vector2 _dragOffset; // fully hidden implementatio details
 
     public void BeginDrag(int squareIndex, Vector2 mousePosition, Vector2 tileOrigin)
     {
-
+        // we are dragging
+        isDragging = true;
+        selectedSquareIndex = squareIndex;
+        _dragOffset = mousePosition - tileOrigin;
+        currentDragPosition = tileOrigin;
     }
     public void UpdateDrag(Vector2 mousePosition)
     {
-
+        
     }
     public void EndDrag(out int squareIndex)
     {
