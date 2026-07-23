151b738a72a246b2cfc73dbf3f2df0b2d3758cf4
Zaido219 <jpmalbas4@gmail.com>
2026-07-23 15:43:05 +0800

    style:WIP adding red overlay for valid move squares

--- src/Chess.UI/IBoardRenderer.cs
+++ src/Chess.UI/IBoardRenderer.cs
@@ -4,6 +4,7 @@ namespace Chess.UI;
 public interface IBoardRenderer
 {
     void Init();
-    void Render(Board board, IDragAndDropHandler dragHandler);
+    // highlightedSquares is an optional propery that refers to valid move squares
+    void Render(Board board, IDragAndDropHandler dragHandler, IEnumerable<int>? highlightedSquares=null);
     void Dispose();
 }
